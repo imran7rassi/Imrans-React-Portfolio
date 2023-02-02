47897754126a720d33e83b1e9f7585610401706c
imran-rassi <imran.rassi.2020@gmail.com>
2023-02-01 20:19:24 -0500

    added the latest updates

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+function Footer() {   
+    return (
+    <footer className="footer bg-dark fixed-bottom">
+        <a href="https://github.com/imran7rassi"><img src="https://img.icons8.com/color/48/000000/github--v1.png" alt="Github" className="icon"/></a>
+        <a href="https://www.linkedin.com/in/imran-rasi-a01a98259/"><img src="https://img.icons8.com/fluent/48/000000/linkedin.png" alt="LinkedIn" className="icon"/></a>
+        <a href="http://twitter.com/ImranRassi"><img src="https://img.icons8.com/color/48/null/twitter--v1.png" alt="twitter" className="icon"/></a>
+    </footer>
+    )
+}
+
+export default Footer;
